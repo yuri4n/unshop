4c8cea6687b2f5524c4f884435a742a0a4519a51
JulianGarzon22 <bryangarzon22@gmail.com>
2019-09-30 10:13:35 -0500

    Organized item routes

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,43 +1,8 @@
 import { Router } from "express";
-import fs from "fs";
-import { Item } from "../item/InterfaceItem";
-const uuid = require("uuid/v4");
+import { itemRouter } from "./item.routes";
 
 const router = Router();
 
-const formatAndWrite = (items: Item[]): void => {
-  const formatItems = JSON.stringify(items, null, 2);
-  fs.writeFileSync("./src/files/items.json", formatItems, "utf-8");
-};
-
-let data = fs.readFileSync("./src/files/items.json", "utf-8");
-let items: Item[] = JSON.parse(data.toString());
-
-router.route("/api").get((req: any, res: any) => {
-  res.json("UNShop API");
-});
-
-router.get("/api/items", (req: any, res: any) => {
-  res.json(items);
-});
-
-router.post("/api/items", (req: any, res: any) => {
-  let { name, description, price } = req.body;
-  const newItem = {
-    id: uuid(),
-    name,
-    description,
-    price
-  };
-  items.push(newItem);
-  formatAndWrite(items);
-  res.send("recibido");
-});
-
-router.get("/api/items/delete/:id", (req: any, res: any) => {
-  items = items.filter((item: Item) => item.id != req.params.id);
-  formatAndWrite(items);
-  res.send("eliminado");
-});
+router.use(itemRouter);
 
 export default router;
---
+++ src/routes/item.routes.ts
@@ -0,0 +1,43 @@
+import { Router, Response, Request } from "express";
+import { Item } from "../item/InterfaceItem";
+import fs from "fs";
+const uuid = require("uuid/v4");
+
+const itemRouter = Router();
+
+const formatAndWrite = (items: Item[]): void => {
+  const formatItems = JSON.stringify(items, null, 2);
+  fs.writeFileSync("./src/files/items.json", formatItems, "utf-8");
+};
+
+let data: string = fs.readFileSync("./src/files/items.json", "utf-8");
+let items: Item[] = JSON.parse(data);
+
+itemRouter.route("/api").get((req: Request, res: Response) => {
+  res.json("UNShop API");
+});
+
+itemRouter.get("/api/items", (req: Request, res: Response) => {
+  res.json(items);
+});
+
+itemRouter.post("/api/items", (req: Request, res: Response) => {
+  let { name, description, price } = req.body;
+  const newItem: Item = {
+    id: uuid(),
+    name,
+    description,
+    price
+  };
+  items.push(newItem);
+  formatAndWrite(items);
+  res.send("recibido");
+});
+
+itemRouter.get("/api/items/delete/:id", (req: Request, res: Response) => {
+  items = items.filter((item: Item) => item.id != req.params.id);
+  formatAndWrite(items);
+  res.send("eliminado");
+});
+
+export { itemRouter };
