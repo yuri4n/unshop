94a211de139adafc6d6cdd5331eb726ad5ba4d5b
JulianGarzon22 <bryangarzon22@gmail.com>
2019-11-17 22:06:50 -0500

    Landing fetch "/api/trending"

--- frontend/src/app/components/Trending/Trending.tsx
+++ frontend/src/app/components/Trending/Trending.tsx
@@ -11,14 +11,14 @@ interface Item {
 export interface Props {}
 
 export interface State {
-  items: Item[] | null;
+  items: Item[];
 }
 
 class Trending extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
     this.state = {
-      items: null
+      items: []
     };
   }
 
@@ -43,111 +43,27 @@ class Trending extends React.Component<Props, State> {
           <span className="trend-title">Tendencias</span>
         </div>
         <div className="items-container">
-          <div className="item-box">
-            <div className="img-box">
-              <img
-                className="img"
-                src="./images/test_items/libro1.png"
-                alt=""
-              />
-            </div>
-            <div className="text-box">
-              <span className="score">4.2</span>
-              <br />
-              <span className="title">
-                Afroanimismo: Memorias de África en la apuesta por la vida
-              </span>
-              <br />
-              <span className="autor">Jaime Arocha</span>
-              <div className="btn">
-                <span className="btn-text">Ver más</span>
+          {this.state.items!.map(item => (
+            <div className="item-box">
+              <div className="img-box">
+                <img
+                  className="img"
+                  src="./images/test_items/libro1.png"
+                  alt=""
+                />
+              </div>
+              <div className="text-box">
+                <span className="score"> {item.price} </span>
+                <br />
+                <span className="title">{item.name}</span>
+                <br />
+                <span className="autor">Jaime Arocha</span>
+                <div className="btn">
+                  <span className="btn-text">Ver más</span>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="item-box">
-            <div className="img-box">
-              <img
-                className="img"
-                src="./images/test_items/libro2.png"
-                alt=""
-              />
-            </div>
-            <div className="text-box">
-              <span className="score">4.2</span>
-              <br />
-              <span className="title">
-                Afroanimismo: Memorias de África en la apuesta por la vida
-              </span>
-              <br />
-              <span className="autor">Jaime Arocha</span>
-              <div className="btn">
-                <span className="btn-text">Ver más</span>
-              </div>
-            </div>
-          </div>
-          <div className="item-box">
-            <div className="img-box">
-              <img
-                className="img"
-                src="./images/test_items/backpack.png"
-                alt=""
-              />
-            </div>
-            <div className="text-box">
-              <span className="score">4.2</span>
-              <br />
-              <span className="title">
-                Afroanimismo: Memorias de África en la apuesta por la vida
-              </span>
-              <br />
-              <span className="autor">Jaime Arocha</span>
-              <div className="btn">
-                <span className="btn-text">Ver más</span>
-              </div>
-            </div>
-          </div>
-          <div className="item-box">
-            <div className="img-box">
-              <img
-                className="img"
-                src="./images/test_items/gadget1.png"
-                alt=""
-              />
-            </div>
-            <div className="text-box">
-              <span className="score">4.2</span>
-              <br />
-              <span className="title">
-                Afroanimismo: Memorias de África en la apuesta por la vida
-              </span>
-              <br />
-              <span className="autor">Jaime Arocha</span>
-              <div className="btn">
-                <span className="btn-text">Ver más</span>
-              </div>
-            </div>
-          </div>
-          <div className="item-box">
-            <div className="img-box">
-              <img
-                className="img"
-                src="./images/test_items/gadget2.jpg"
-                alt=""
-              />
-            </div>
-            <div className="text-box">
-              <span className="score">4.2</span>
-              <br />
-              <span className="title">
-                Afroanimismo: Memorias de África en la apuesta por la vida
-              </span>
-              <br />
-              <span className="autor">Jaime Arocha</span>
-              <div className="btn">
-                <span className="btn-text">Ver más</span>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     );
